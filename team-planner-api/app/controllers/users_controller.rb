9f7641c6ed53f15951bfd9225583c6c6a2b540ab
lukevangorder <lukevangorder@gmail.com>
2021-05-07 17:37:09 -0400

    Adds user create route to api and user_arams permissions

--- team-planner-api/app/controllers/users_controller.rb
+++ team-planner-api/app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
 
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :role)
+    end
+    
 end
