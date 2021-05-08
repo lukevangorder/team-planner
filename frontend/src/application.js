b684a2c28a488259b3076871ae7c979e79ee1801
lukevangorder <lukevangorder@gmail.com>
2021-05-08 02:15:21 -0400

    Adds event listener for next and previous user buttons

--- frontend/src/application.js
+++ frontend/src/application.js
@@ -1,8 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    
     let user = 1;
+
+    // Fetch request to get list of all users
+    fetch(`http://localhost:3000/users`) .then(function(response) {
+        return response.json();
+    }) .then(function(json){
+        let users = json;
+    });
+
     fetch(`http://localhost:3000/events/${user}`) .then(function(response) {
         return response.json();
     }) .then(function(json){
-        console.log(json);
+        let events = json;
+        console.log(events);
     });
+
+    function grabEvents() {
+        fetch(`http://localhost:3000/events/${user}`) .then(function(response) {
+            return response.json();
+        }) .then(function(json){
+            let events = json;
+            console.log(events);
+        });
+    }
+
+    function nextUser() {
+        user += 1;
+        grabEvents();
+    }
+
+    function previousUser() {
+        user -= 1;
+        grabEvents();
+    }
+
+    document.getElementById('previous').addEventListener('click', previousUser());
+    document.getElementById('next').addEventListener('click', nextUser());
 });
--- team-planner-api/app/models/event.rb
+++ team-planner-api/app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
     
     belongs_to :user
 
+    def day_of_week
+        self.starts_at.strftime('%u').to_i;
+    end
+
 end
