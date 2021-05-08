216eaa1da0de9a5348c2bc8c1e1b7002d8a9f1c9
lukevangorder <lukevangorder@gmail.com>
2021-05-07 23:31:25 -0400

    Adds show and index routes to events controller

--- team-planner-api/app/controllers/events_controller.rb
+++ team-planner-api/app/controllers/events_controller.rb
@@ -1,18 +1,28 @@
 class EventsController < ApplicationController
 
     def create
-        user = User.new(user_params)
-        if user.save
-            render json: user
+        event = Event.new(event_params)
+        if event.save
+            render json: event
         else
-            render json: user.errors
+            render json: event.errors
         end
+    end
+
+    def index
+        events = Event.all
+        render json: events
+    end
+
+    def show #shows all by user
+        events = Event.where(user_id: params[:id])
+        render json: events
     end
 
     private
 
-    def user_params
-        params.require(:user).permit(:name, :role)
+    def event_params
+        params.require(:event).permit(:name, :info, :starts_at, :ends_at, :user)
     end
     
 end
