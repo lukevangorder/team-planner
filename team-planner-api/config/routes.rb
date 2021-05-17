b06fad28b8bf358df589fc8f6c345156d3b59659
lukevangorder <lukevangorder@gmail.com>
2021-05-17 06:24:49 -0400

    Adds update events route to routes.rb

--- team-planner-api/config/routes.rb
+++ team-planner-api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :index]
-  resources :events, only: [:create, :index, :show]
+  resources :events, only: [:create, :index, :show, :update]
 
 end
